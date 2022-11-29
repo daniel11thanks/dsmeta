1f707f02e6854e0de3ab38f8da0d09cb9ee26777
daniel11thanks <daniel11thanks@gmail.com>
2022-11-28 21:55:47 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./assets/components/Header";
 import Salescard from "./assets/components/Salescard";
 
@@ -5,6 +7,7 @@ import Salescard from "./assets/components/Salescard";
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
